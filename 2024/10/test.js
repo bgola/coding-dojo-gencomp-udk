4bd8bcb5d4c63976feb4e8037659527a1c5ef2c2
Bruno Gola <me@bgo.la>
2024-10-23 13:12:31 +0200

    OK

--- 2024/10/test.js
+++ 2024/10/test.js
@@ -8,11 +8,18 @@ glitch_live = (frame) => {
 	const fwd_mvs = frame.mv?.forward;
 
 	if ( fwd_mvs ) {
-		fwd_mvs.forEach((mv, i, j) => {
+		// go over all motion vectors (2D)
+		fwd_mvs.forEach((mv, row, col) => {
 			fwd_mvs.width;
-			fwd_mvs.height;
-			let shiftX = fwd_mvs.height/2 - i;
-			mv[0] += shiftX;
+			let spinspeed = 0.2;
+			// calculate how much to shift each column:
+			// begin with half height for left, 
+			// and go down to -half height on right.
+			let shiftX = fwd_mvs.height/2 - row;
+			mv[0] += shiftX * spinspeed;
+			let shiftY = fwd_mvs.width/2 - col;
+			mv[1] -= shiftY *  spinspeed;
+			
 		})
 	}
 }
